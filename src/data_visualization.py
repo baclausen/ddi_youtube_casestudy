792ad68df667fafd72f2cb1417dbacc1013844ac
baclausen <burton.clausen@gmail.com>
2025-10-23 23:16:31 -0400

    adding notebook file with cleaned data

--- src/data_visualization.py
+++ src/data_visualization.py
@@ -1,1 +1,14 @@
  # Matplotlib/Seaborn charts
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+# Import our data files
+df = pd.read_csv('../data/USvideos.csv')
+df_cat = pd.read_json('../data/US_category_id.json')
+
+# Heatmap of numerica data
+sns.heatmap(df.corr(numeric_only=True), annot=True, cmap='coolwarm')
+plt.title('YouTube Numerical Statistics Correlations')
+plt.show()
